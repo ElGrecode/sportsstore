7e52ce2cb1ce347c1aa8c4a7002b31c7a61c7878
narwhaldo <narwhaldo@gmail.com>
2015-02-05 16:45:59 -0800

    API endpoint error handling

--- angularjs/controllers/sportsStore.js
+++ angularjs/controllers/sportsStore.js
@@ -1,16 +1,14 @@
 angular.module("sportsStore")
-.controller("sportsStoreCtrl", function ($scope) {
-    $scope.data = {
-        products: [
-		{ name: "Product #1", description: "A product",
-		    category: "Category #1", price: 100 },
-		{ name: "Product #2", description: "A product",
-		    category: "Category #1", price: 110 },
-		{ name: "Product #3", description: "A product",
-		    category: "Category #2", price: 210 },
-		{ name: "Product #4", description: "A product",
-		    category: "Category #3", price: 202 },
-		{ name: "Product #4", description: "A product",
-		    category: "Sports", price: 202 }]
-		};
+.constant("dataUrl", "http://localhost:5500/products")
+.controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+    $scope.data = {};
+
+    // query for product data
+    $http.get(dataUrl)
+    	.success(function (data){
+    		$scope.data.products = data;
+    	})
+    	.error(function (error){
+    		$scope.data.error = error;
+    	});
 });
