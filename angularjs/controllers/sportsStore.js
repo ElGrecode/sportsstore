0c2791d791ae0a9f9263b5d26bf99fd2817bc377
narwhaldo <narwhaldo@gmail.com>
2015-02-07 10:13:23 -0800

    full process of checkout and saving of orders

--- angularjs/controllers/sportsStore.js
+++ angularjs/controllers/sportsStore.js
@@ -1,6 +1,8 @@
 angular.module("sportsStore")
 .constant("dataUrl", "http://localhost:5500/products")
-.controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
+.constant("orderUrl", "http://localhost:5500/orders")
+.controller("sportsStoreCtrl", function ($scope, $http, $location,
+	dataUrl, orderUrl, cart) {
     $scope.data = {};
 
     // query for product data
@@ -11,4 +13,20 @@ angular.module("sportsStore")
     	.error(function (error){
     		$scope.data.error = error;
     	});
+
+    $scope.sendOrder = function( shippingDetails ){
+    	var order = angular.copy(shippingDetails);
+    	order.products = cart.getProducts();
+    	$http.post(orderUrl, order)
+    		.success(function(data){
+    			$scope.data.orderId = data.id;
+    			cart.getProducts().length = 0;
+    		})
+    		.error(function(error){
+    			$scope.data.orderError = error;
+    		})
+    		.finally(function(){
+    			$location.path("/complete");
+    		});
+    };
 });
