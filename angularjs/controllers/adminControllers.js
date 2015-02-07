c836e3cb28e4f0424a6c91ab37d4d03eb53b60a4
narwhaldo <narwhaldo@gmail.com>
2015-02-07 11:48:58 -0800

    creates main admin controller

--- angularjs/controllers/adminControllers.js
+++ angularjs/controllers/adminControllers.js
@@ -13,4 +13,20 @@ angular.module("sportsStoreAdmin")
                 $scope.authenticationError = error;
             });
         }
+    })
+    .controller("mainCtrl", function($scope){
+
+    	$scope.screens = ["Products", "Orders"];
+    	$scope.current = $scope.screens[0];
+
+    	$scope.setScreen = function(index){
+    		$scope.current = $scope.screens[index];
+    	};
+
+    	$scope.getScreen = function(){
+    		return $scope.current == "Products"
+    			? "/views/adminProducts.html" : "/views/adminOrders.html";
+
+    	};
+
     });
